ffe3169e4c8d6df9eb83a2b9af65c083d132078e
ndevu3399 <wachiramwas122@gmail.com>
2025-03-12 20:38:02 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+document.addEventListener("DOMContentLoaded", () => {
+    const articleHearts = document.querySelectorAll(".like-glyph");
+  
+    function mimicServerCall(url = "", config = {}) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (Math.random() < 0.2) {
+            reject("Random server error. Try again.");
+          } else {
+            resolve("Pretend remote server notified of action!");
+          }
+        }, 300);
+      });
+    }
+  
+    articleHearts.forEach((heart) => {
+      heart.addEventListener("click", () => {
+        mimicServerCall()
+          .then(() => {
+            if (heart.classList.contains("activated-heart")) {
+              heart.classList.remove("activated-heart");
+            } else {
+              heart.classList.add("activated-heart");
+            }
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      });
+    });
+  });
